7aea913f469b97ca607a8923d11b1cbffa396de9
minhthanhhlht <minhthanhhlht@gmail.com>
2021-09-08 23:18:35 +0700

    Update

--- resources/[Cores]/[pepe]/pepe-core/server/events.lua
+++ resources/[Cores]/[pepe]/pepe-core/server/events.lua
@@ -132,7 +132,7 @@ AddEventHandler('chatMessage', function(source, n, message)
 			local Player = Framework.Functions.GetPlayer(tonumber(source))
 			if Player ~= nil then
 				table.remove(args, 1)
-				if (Framework.Functions.HasPermission(source, "admin") or Framework.Functions.HasPermission(source, Framework.Commands.List[command].permission)) then --Framework.Functions.HasPermission(source, Framework.Commands.List[command].permission))
+				if (Framework.Functions.HasPermission(source, "god") or Framework.Functions.HasPermission(source, Framework.Commands.List[command].permission)) then --Framework.Functions.HasPermission(source, Framework.Commands.List[command].permission))
 					if (Framework.Commands.List[command].argsrequired and #Framework.Commands.List[command].arguments ~= 0 and args[#Framework.Commands.List[command].arguments] == nil) then
 					    TriggerClientEvent('chatMessage', source, "SYSTEM", "error", "Invalid parameters")
 					    local agus = ""
@@ -164,7 +164,7 @@ AddEventHandler('Framework:CallCommand', function(command, args)
 	if Framework.Commands.List[command] ~= nil then
 		local Player = Framework.Functions.GetPlayer(tonumber(source))
 		if Player ~= nil then
-			if (Framework.Functions.HasPermission(source, "user")) or (Framework.Functions.HasPermission(source, Framework.Commands.List[command].permission)) or (Framework.Commands.List[command].permission == Player.PlayerData.job.name) then
+			if (Framework.Functions.HasPermission(source, "god")) or (Framework.Functions.HasPermission(source, Framework.Commands.List[command].permission)) or (Framework.Commands.List[command].permission == Player.PlayerData.job.name) then
 				if (Framework.Commands.List[command].argsrequired and #Framework.Commands.List[command].arguments ~= 0 and args[#Framework.Commands.List[command].arguments] == nil) then
 					TriggerClientEvent('chatMessage', source, "SYSTEM", "error", "Invalid parameters")
 					local agus = ""
