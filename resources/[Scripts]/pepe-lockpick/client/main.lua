2b7b47dc3594f9e8369254106eff926ef1d87410
minhthanhhlht <minhthanhhlht@gmail.com>
2021-09-11 16:22:24 +0700

    Update lockpick

--- resources/[Scripts]/pepe-lockpick/client/main.lua
+++ resources/[Scripts]/pepe-lockpick/client/main.lua
@@ -1,183 +1,55 @@
--- local IsLockPicking = false
+local IsLockPicking = false
 
--- Framework = nil
+Framework = nil
 
--- RegisterNetEvent('Framework:Client:OnPlayerLoaded')
--- AddEventHandler('Framework:Client:OnPlayerLoaded', function()
---  Citizen.SetTimeout(1250, function()
---      TriggerEvent("Framework:GetObject", function(obj) Framework = obj end)    
--- 	   Citizen.Wait(250)
---  end)
--- end)
+RegisterNetEvent('Framework:Client:OnPlayerLoaded')
+AddEventHandler('Framework:Client:OnPlayerLoaded', function()
+ Citizen.SetTimeout(1250, function()
+     TriggerEvent("Framework:GetObject", function(obj) Framework = obj end)    
+	   Citizen.Wait(250)
+ end)
+end)
 
--- -- Code
+-- Code
 
--- AddEventHandler('pepe-lockpick:client:openLockpick', function(callback)
+AddEventHandler('pepe-lockpick:client:openLockpick', function(callback)
+    lockpickCallback = callback
+    openLockpick(true)
+end)
+
+-- AddEventHandler('kwk-lockpick:client:openLockpick', function(callback)
 --     lockpickCallback = callback
---     openLockpick(true)
+--     exports['pepe-lock']:StartLockPickCircle(total)
 -- end)
 
--- function OpenLockpickGame(callback)
---  lockpickCallback = callback
---  openLockpick(true)
--- end
+function OpenLockpickGame(callback)
+ lockpickCallback = callback
+ openLockpick(true)
+end
 
--- RegisterNUICallback('callback', function(data, cb)
---     openLockpick(false)
--- 	lockpickCallback(data.success)
---     cb('ok')
--- end)
+RegisterNUICallback('callback', function(data, cb)
+    openLockpick(false)
+	lockpickCallback(data.success)
+    cb('ok')
+end)
 
--- RegisterNUICallback('exit', function(data)
---     lockpickCallback(data.success)
---     openLockpick(false)
--- end)
+RegisterNUICallback('exit', function(data)
+    lockpickCallback(data.success)
+    openLockpick(false)
+end)
 
---  function openLockpick(bool)
---  SetNuiFocus(bool, bool)
---  SendNUIMessage({
---      action = "ui",
---      toggle = bool,
---  })
---  SetCursorLocation(0.5, 0.2)
---  IsLockPicking = bool
--- end
+ function openLockpick(bool)
+ SetNuiFocus(bool, bool)
+ SendNUIMessage({
+     action = "ui",
+     toggle = bool,
+ })
+ SetCursorLocation(0.5, 0.2)
+ IsLockPicking = bool
+end
 
--- function GetLockPickStatus()
---     return IsLockPicking
--- end
+function GetLockPickStatus()
+    return IsLockPicking
+end
 
 --// Code New //--
-Framework = nil
-Citizen.CreateThread(function() 
-    while true do
-        Citizen.Wait(10)
-        if Framework == nil then
-            TriggerEvent("Framework:GetObject", function(obj) Framework = obj end)    
-            Citizen.Wait(200)
-        end
-    end
-end)
-local Result = nil
-local NUI_status = false
-
-AddEventHandler('kwk-lockpick:client:openLockpick', function(callback)
-    lockpickCallback = callback
-    exports['pepe-lockpick']:StartLockPickCircle(total)
-end)
-
-function StartLockPickCircle(callback)
-    if IsPedInAnyVehicle(PlayerPedId(), false) and GetPedInVehicleSeat(GetVehiclePedIsIn(PlayerPedId(), true), -1) == PlayerPedId() then
-
-    Result = nil
-    NUI_status = true
-    SendNUIMessage({
-        action = 'ui',
-        value = 4,
-    })
-    while NUI_status do
-        Wait(5)
-        SetNuiFocus(NUI_status, NUI_status)
-    end
-    Wait(100)
-    SetNuiFocus(false, false)
-    lockpickCallback = callback
-    print(lockpickCallback)
-    return Result
-end
-end
-
-RegisterNUICallback('fail', function()
-        if math.random(1, 100) < 23 then
-            TriggerServerEvent("Framework:Server:RemoveItem", "lockpick", 1)
-            TriggerEvent('pepe-inventory:client:ItemBox', Framework.Shared.Items["lockpick"], "remove")
-        end
-        
-        ClearPedTasks(PlayerPedId())
-        Result = false
-        Wait(100)
-        NUI_status = false
-        --print('fail')
-end)
-
-
-local openingDoor = false
-RegisterNUICallback('success', function()
-    local vehicle = Framework.Functions.GetClosestVehicle()
-    if IsPedInAnyVehicle(PlayerPedId(), false) and GetPedInVehicleSeat(GetVehiclePedIsIn(PlayerPedId(), true), -1) == PlayerPedId() then
-
-        --if vehicle ~= nil then
-        local lockpickTime = 2000
-        LockpickDoorAnim(lockpickTime)
-        Framework.Functions.Progressbar("veh_slot", "Đang phá khoá xe...", lockpickTime, false, true, {
-            disableMovement = true,
-            disableCarMovement = true,
-            disableMouse = false,
-            disableCombat = true,
-        }, {
-            animDict = "veh@break_in@0h@p_m_one@",
-            anim = "low_force_entry_ds",
-            flags = 16,
-        }, {}, {}, function() -- Done
-            openingDoor = false
-            ClearPedTasks(PlayerPedId())
-            Framework.Functions.Notify("Lockpick thành công!", 'success')
-            HasKey = true
-            SetVehicleDoorsLocked(Vehicle, 1)
-            Framework.Functions.Notify("Phương tiện bị khóa!", 'success')
-            TriggerEvent('pepe-vehicleley:client:blink:lights', Vehicle)
-            TriggerServerEvent("pepe-sound:server:play:distance", 5, "car-unlock", 0.2)
-            StopAnimTask(PlayerPedId(), "anim@amb@clubhouse@tutorial@bkr_tut_ig3@", "machinic_loop_mechandplayer", 1.0)
-            IsHotwiring = false
-            exports['pepe-vehiclekeys']:SetVehicleKey(GetVehicleNumberPlateText(vehicle), true)
-            SetVehicleDoorsLocked(vehicle, 1)
-            SetVehicleDoorsLockedForAllPlayers(vehicle, false)
-            SetVehicleEngineOn(vehicle, false, false, true)
-
-        end, function() -- Cancel
-            openingDoor = false
-            ClearPedTasks(PlayerPedId())
-            Framework.Functions.Notify("Lockpick hủy bỏ..", "error")
-        end)
-        Citizen.CreateThread(function()
-            while openingDoor do
-                TriggerServerEvent('pepe-hud:Server:GainStress', math.random(1, 2))
-                Citizen.Wait(10000)
-            end
-        end)
-        Result = true
-        Wait(100)
-        NUI_status = false
-        
-    SetNuiFocus(false, false)
-    print(Result)
-    return Result
-    end
-end)
-
-
-function LockpickDoorAnim(time)
-    time = time / 1000
-    loadAnimDict("veh@break_in@0h@p_m_one@")
-    TaskPlayAnim(PlayerPedId(), "veh@break_in@0h@p_m_one@", "low_force_entry_ds" ,3.0, 3.0, -1, 16, 0, false, false, false)
-    openingDoor = true
-    Citizen.CreateThread(function()
-        while openingDoor do
-            TaskPlayAnim(PlayerPedId(), "veh@break_in@0h@p_m_one@", "low_force_entry_ds", 3.0, 3.0, -1, 16, 0, 0, 0, 0)
-            Citizen.Wait(2000)
-            time = time - 2
-            if time <= 0 then
-                openingDoor = false
-                StopAnimTask(PlayerPedId(), "veh@break_in@0h@p_m_one@", "low_force_entry_ds", 1.0)
-            end
-        end
-    end)
-end
-
-
-function loadAnimDict( dict )
-    while ( not HasAnimDictLoaded( dict ) ) do
-        RequestAnimDict( dict )
-        Citizen.Wait( 0 )
-    end
-end
